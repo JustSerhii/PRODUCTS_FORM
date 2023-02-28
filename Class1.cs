cdf9df4a2862247c733b1d62b6f1ebe128381412
Serhii <scimitarnew@gmail.com>
2023-02-28 21:54:32 +0200

    commit0

---
+++ Class1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task
+{
+    public class Good
+    {
+        public int Price { get; set; }
+        public string Country { get; set; }
+        public string Name { get; set; }
+        public string Date { get; set; }
+        public string Description { get; set; }
+
+        public Good(int price, string country, string name, string date, string description)
+        {
+            Price = price;
+            Country = country;
+            Name = name;
+            Date = date;
+            Description = description;
+        }
+    }
+    public class Books : Good
+    {
+        private readonly int _pageAmount;
+        private readonly string _publisher;
+        private readonly string _author;
+
+        public Books(string name, int price, string country, string date, string description,
+        int pageAmount, string publisher, string author) : base(price, country, name, date, description)
+        {
+            _pageAmount = pageAmount;
+            _publisher = publisher;
+            _author = author;
+        }
+    }
+    public class Products : Good
+    {
+        private readonly string _shelfLife;
+        private readonly int _count;
+        private readonly string _measure;
+
+        public Products(string name, int price, string country, string date,
+        string description, string shelfLife, int count, string measure) : base(price, country, name, date, description)
+        {
+            _shelfLife = shelfLife;
+            _count = count;
+            _measure = measure;
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task
+{
+    public partial class Form1 : Form
+    {
+
+        DataTable table1 = new DataTable();
+        List<Good> list = new List<Good>();
+        public List<Good> Goods { get; set; }
+        public Form1()
+        {
+            Goods = GetGoods();
+            InitializeComponent();
+        }
+
+        private List<Good> GetGoods()
+        {
+            return list;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+       
+            table1.Columns.Add("Name", typeof(string));
+            table1.Columns.Add("Price", typeof(int));
+            table1.Columns.Add("Country", typeof(string));
+            table1.Columns.Add("Packing date", typeof(string));
+            table1.Columns.Add("Description", typeof(string));
+
+            Books book1 = new Books("The Fellowship of the Ring", 20, "England", "21.09.2022", "Book", 423, "George Allen & Unwin", "John Ronald Reuel Tolkien");
+            list.Add(book1);
+
+            Products product1 = new Products("Skittles", 5, "USA", "04.10.2022", "Sweets", "04.11.2022", 20, "kg");
+            list.Add(product1);
+            
+
+            dataGridView1.DataSource = table1;
+
+
+            for (int i = 0; i < Goods.Count; i++)
+            {
+                table1.Rows.Add(Goods[i].Name, Goods[i].Price, Goods[i].Country, Goods[i].Date, Goods[i].Description);
+            }
+        }
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.RemoveAt(dataGridView1.Rows.Count - 2);
+
+        }
+    }
+}
